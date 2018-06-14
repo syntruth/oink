4786920432400849bd0b6f9fc8dd5a889d8ad784
Randy Carnahan <randy.carnahan@charter.com>
2018-06-14 11:15:51 -0600

    Updated Middleware to handle non-rails routing info
    This will now always include a routing info line in the logs. If it's
    unable to determined a Controller#Action for rails, it'll check
    PATH_INFO from the Rack env, and if that fails, then it'll return
    'Unknown'.
    
    Also various clean-up changes for readability. :)

--- lib/oink/middleware.rb
+++ lib/oink/middleware.rb
@@ -4,11 +4,17 @@ require 'oink/instrumentation'
 
 module Oink
   class Middleware
-
     def initialize(app, options = {})
-      @app         = app
-      @logger      = options[:logger] || Hodel3000CompliantLogger.new("log/oink.log")
-      @instruments = options[:instruments] ? Array(options[:instruments]) : [:memory, :activerecord]
+      @app = app
+
+      @logger = options[:logger] ||
+                Hodel3000CompliantLogger.new('log/oink.log')
+
+      @instruments = if options[:instruments]
+                       Array(options[:instruments])
+                     else
+                       %i[memory activerecord]
+                     end
 
       Oink.extend_active_record! if @instruments.include?(:activerecord)
     end
@@ -20,39 +26,52 @@ module Oink
       log_memory
       log_activerecord
       log_completed
+
       [status, headers, body]
     end
 
     def log_completed
-      @logger.info("Oink Log Entry Complete")
+      @logger.info('Oink Log Entry Complete')
     end
 
     def log_routing(env)
-      routing_info = rails3_routing_info(env) || rails2_routing_info(env)
-      if routing_info
-        controller = routing_info['controller']
-        action     = routing_info['action']
-        @logger.info("Oink Action: #{controller}##{action}")
-      end
+      @logger.info("Oink Action: #{routing_info_string(env)}")
     end
 
     def log_memory
-      if @instruments.include?(:memory)
-        memory = Oink::Instrumentation::MemorySnapshot.memory
-        @logger.info("Memory usage: #{memory} | PID: #{$$}")
-      end
+      return unless @instruments.include?(:memory)
+
+      memory = Oink::Instrumentation::MemorySnapshot.memory
+      @logger.info("Memory usage: #{memory} | PID: #{$$}")
     end
 
     def log_activerecord
-      if @instruments.include?(:activerecord)
-        sorted_list = Oink::HashUtils.to_sorted_array(ActiveRecord::Base.instantiated_hash)
-        sorted_list.unshift("Total: #{ActiveRecord::Base.total_objects_instantiated}")
-        @logger.info("Instantiation Breakdown: #{sorted_list.join(' | ')}")
-        reset_objects_instantiated
-      end
+      return unless @instruments.include?(:activerecord)
+
+      hash    = ActiveRecord::Base.instantiated_hash
+      objects = ActiveRecord::Base.total_objects_instantiated
+
+      sorted_list = ["Total: #{objects}"] +
+                    Oink::HashUtils.to_sorted_array(hash)
+
+      @logger.info("Instantiation Breakdown: #{sorted_list.join(' | ')}")
+
+      reset_objects_instantiated
     end
 
-  private
+    private
+
+    def routing_info_string(env)
+      rails_routing_info(env) || raw_routing_info(env) || 'Unknown'
+    end
+
+    def rails_routing_info(env)
+      info = rails3_routing_info(env) || rails2_routing_info(env)
+
+      return unless info
+
+      "#{info['controller']}##{info['action']}"
+    end
 
     def rails3_routing_info(env)
       env['action_dispatch.request.parameters']
@@ -62,9 +81,12 @@ module Oink
       env['action_controller.request.path_parameters']
     end
 
+    def raw_routing_info(env)
+      env['PATH_INFO']
+    end
+
     def reset_objects_instantiated
       ActiveRecord::Base.reset_instance_type_count
     end
-
   end
 end
